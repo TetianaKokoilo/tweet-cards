44d80f6c78f8771b41fa02fcbc59e2dd29bdeb57
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-04-25 20:42:55 +0300

    Update TweetCard.styled.js

--- src/components/TweetCard/TweetCard.styled.js
+++ src/components/TweetCard/TweetCard.styled.js
@@ -10,31 +10,51 @@ export const StyledContainer = styled.div`
   margin-top: 10px;
 `;
 export const StyledRectangle = styled.div`
+  position: relative;
   margin-top: 150px;
   height: 8px;
   background: #ebd8ff;
   box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.06),
     inset 0px -1.71846px 3.43693px #ae7be3, inset 0px 3.43693px 2.5777px #fbf8ff;
 `;
+
 export const StyledBoy = styled.img`
   position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  top: 170px;
   width: 80px;
   height: 80px;
-  left: 150px;
-  top: 178px;
-  /* margin: 0 auto;
-  display: block; */
   border-radius: 85.9232px;
-  @media (min-width: 768px) {
-    left: 350px;
-  }
-  @media (min-width: 1024px) {
-    left: 480px;
-  }
-@media (min-width: 1440px) {
-    left: 730px;
-  }
 `;
+
+
+// export const StyledRectangle = styled.div`
+//   margin-top: 150px;
+//   height: 8px;
+//   background: #ebd8ff;
+//   box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.06),
+//     inset 0px -1.71846px 3.43693px #ae7be3, inset 0px 3.43693px 2.5777px #fbf8ff;
+// `;
+// export const StyledBoy = styled.img`
+//   position: absolute;
+//   width: 80px;
+//   height: 80px;
+//   left: 150px;
+//   top: 178px;
+//   /* margin: 0 auto;
+//   display: block; */
+//   border-radius: 85.9232px;
+//   @media (min-width: 768px) {
+//     left: 350px;
+//   }
+//   @media (min-width: 1024px) {
+//     left: 480px;
+//   }
+// @media (min-width: 1440px) {
+//     left: 730px;
+//   }
+// `;
 export const StyledLogo = styled.div`
   margin-left: 20px;
   padding-top: 20px;
